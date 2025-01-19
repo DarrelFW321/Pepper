6ba9965ef5154c25203299dcb899338ca84d1f0c
DarrelFW321 <darrelwihandi123@gmail.com>
2025-01-18 20:49:59 -0500

    added strings and tables

--- compiler.c
+++ compiler.c
@@ -4,6 +4,7 @@
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
+#include "object.h"
 
 #ifdef DEBUG_PRINT_CODE
 #include "debug.h"
@@ -113,11 +114,11 @@ static void emitByte(uint8_t byte)
     writeChunk(currentChunk(), byte, parser.previous.line);
 }
 
-// static void emitBytes(uint8_t byte1, uint8_t byte2)
-// {
-//     emitByte(byte1);
-//     emitByte(byte2);
-// }
+static void emitBytes(uint8_t byte1, uint8_t byte2)
+{
+    emitByte(byte1);
+    emitByte(byte2);
+}
 
 static void emitReturn()
 {
@@ -152,6 +153,24 @@ static void binary()
 
     switch (operatorType)
     {
+    case TOKEN_BANG_EQUAL:
+        emitBytes(OP_EQUAL, OP_NOT);
+        break;
+    case TOKEN_EQUAL_EQUAL:
+        emitByte(OP_EQUAL);
+        break;
+    case TOKEN_GREATER:
+        emitByte(OP_GREATER);
+        break;
+    case TOKEN_GREATER_EQUAL:
+        emitBytes(OP_LESS, OP_NOT);
+        break;
+    case TOKEN_LESS:
+        emitByte(OP_LESS);
+        break;
+    case TOKEN_LESS_EQUAL:
+        emitBytes(OP_GREATER, OP_NOT);
+        break;
     case TOKEN_PLUS:
         emitByte(OP_ADD);
         break;
@@ -169,6 +188,24 @@ static void binary()
     }
 }
 
+static void literal()
+{
+    switch (parser.previous.type)
+    {
+    case TOKEN_FALSE:
+        emitByte(OP_FALSE);
+        break;
+    case TOKEN_NIL:
+        emitByte(OP_NIL);
+        break;
+    case TOKEN_TRUE:
+        emitByte(OP_TRUE);
+        break;
+    default:
+        return; // Unreachable.
+    }
+}
+
 static void grouping()
 {
     expression();
@@ -178,7 +215,13 @@ static void grouping()
 static void number()
 {
     double value = strtod(parser.previous.start, NULL);
-    emitConstant(value);
+    emitConstant(NUMBER_VAL(value));
+}
+
+static void string()
+{
+    emitConstant(OBJ_VAL(copyString(parser.previous.start + 1,
+                                    parser.previous.length - 2)));
 }
 
 static void unary()
@@ -191,6 +234,9 @@ static void unary()
     // Emit the operator instruction.
     switch (operatorType)
     {
+    case TOKEN_BANG:
+        emitByte(OP_NOT);
+        break;
     case TOKEN_MINUS:
         emitByte(OP_NEGATE);
         break;
@@ -211,31 +257,31 @@ ParseRule rules[] = {
     [TOKEN_SEMICOLON] = {NULL, NULL, PREC_NONE},
     [TOKEN_SLASH] = {NULL, binary, PREC_FACTOR},
     [TOKEN_STAR] = {NULL, binary, PREC_FACTOR},
-    [TOKEN_BANG] = {NULL, NULL, PREC_NONE},
-    [TOKEN_BANG_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_BANG] = {unary, NULL, PREC_NONE},
+    [TOKEN_BANG_EQUAL] = {NULL, binary, PREC_EQUALITY},
     [TOKEN_EQUAL] = {NULL, NULL, PREC_NONE},
-    [TOKEN_EQUAL_EQUAL] = {NULL, NULL, PREC_NONE},
-    [TOKEN_GREATER] = {NULL, NULL, PREC_NONE},
-    [TOKEN_GREATER_EQUAL] = {NULL, NULL, PREC_NONE},
-    [TOKEN_LESS] = {NULL, NULL, PREC_NONE},
-    [TOKEN_LESS_EQUAL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_EQUAL_EQUAL] = {NULL, binary, PREC_EQUALITY},
+    [TOKEN_GREATER] = {NULL, binary, PREC_COMPARISON},
+    [TOKEN_GREATER_EQUAL] = {NULL, binary, PREC_COMPARISON},
+    [TOKEN_LESS] = {NULL, binary, PREC_COMPARISON},
+    [TOKEN_LESS_EQUAL] = {NULL, binary, PREC_COMPARISON},
     [TOKEN_IDENTIFIER] = {NULL, NULL, PREC_NONE},
-    [TOKEN_STRING] = {NULL, NULL, PREC_NONE},
+    [TOKEN_STRING] = {string, NULL, PREC_NONE},
     [TOKEN_NUMBER] = {number, NULL, PREC_NONE},
     [TOKEN_AND] = {NULL, NULL, PREC_NONE},
     [TOKEN_CLASS] = {NULL, NULL, PREC_NONE},
     [TOKEN_ELSE] = {NULL, NULL, PREC_NONE},
-    [TOKEN_FALSE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_FALSE] = {literal, NULL, PREC_NONE},
     [TOKEN_FOR] = {NULL, NULL, PREC_NONE},
     [TOKEN_FUN] = {NULL, NULL, PREC_NONE},
     [TOKEN_IF] = {NULL, NULL, PREC_NONE},
-    [TOKEN_NIL] = {NULL, NULL, PREC_NONE},
+    [TOKEN_NIL] = {literal, NULL, PREC_NONE},
     [TOKEN_OR] = {NULL, NULL, PREC_NONE},
     [TOKEN_PRINT] = {NULL, NULL, PREC_NONE},
     [TOKEN_RETURN] = {NULL, NULL, PREC_NONE},
     [TOKEN_SUPER] = {NULL, NULL, PREC_NONE},
     [TOKEN_THIS] = {NULL, NULL, PREC_NONE},
-    [TOKEN_TRUE] = {NULL, NULL, PREC_NONE},
+    [TOKEN_TRUE] = {literal, NULL, PREC_NONE},
     [TOKEN_VAR] = {NULL, NULL, PREC_NONE},
     [TOKEN_WHILE] = {NULL, NULL, PREC_NONE},
     [TOKEN_ERROR] = {NULL, NULL, PREC_NONE},
@@ -281,8 +327,7 @@ bool compile(const char *source, Chunk *chunk)
     parser.panicMode = false;
 
     advance();
-    expression();
-    consume(TOKEN_EOF, "Expect end of expression.");
+
     endCompiler();
 
     return !parser.hadError;
--- vm.c
+++ vm.c
@@ -1,8 +1,13 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
 #include "common.h"
 #include "debug.h"
 #include "vm.h"
 #include "compiler.h"
-#include <stdio.h>
+#include "object.h"
+#include "memory.h"
 
 VM vm;
 
@@ -11,13 +16,31 @@ static void resetStack()
     vm.stackTop = vm.stack;
 }
 
+static void runtimeError(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+    fputs("\n", stderr);
+
+    size_t instruction = vm.ip - vm.chunk->code - 1;
+    int line = getLine(vm.chunk, instruction);
+    fprintf(stderr, "[line %d] in script\n", line);
+    resetStack();
+}
+
 void initVM()
 {
     resetStack();
+    vm.objects = NULL;
+    initTable(&vm.strings);
 }
 
 void freeVM()
 {
+    freeTable(&vm.strings);
+    freeObjects();
 }
 
 void push(Value value)
@@ -30,6 +53,62 @@ Value pop()
 {
     vm.stackTop--;
     return *vm.stackTop;
+}
+
+static Value peek(int distance)
+{
+    return vm.stackTop[-1 - distance];
+}
+
+static bool isFalsey(Value value)
+{
+    return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
+static ObjString *stringifyAndConcatenateWithNumber(double num, ObjString *str, bool numberFirst)
+{
+    // Convert the number to a string
+    char numStr[50];
+    snprintf(numStr, sizeof(numStr), "%g", num); // Converts double to string
+
+    int numLength = (int)strlen(numStr);
+    int strLength = str->length;
+    int totalLength = numLength + strLength;
+
+    // Allocate memory for the concatenated string
+    char *chars = ALLOCATE(char, totalLength + 1);
+
+    if (numberFirst)
+    {
+        // Concatenate the number first, then the string
+        memcpy(chars, numStr, numLength);
+        memcpy(chars + numLength, str->chars, strLength);
+    }
+    else
+    {
+        // Concatenate the string first, then the number
+        memcpy(chars, str->chars, strLength);
+        memcpy(chars + strLength, numStr, numLength);
+    }
+    chars[totalLength] = '\0'; // Null-terminate the result
+
+    // Create a new ObjString for the result
+    return takeString(chars, totalLength);
+}
+
+static void concatenate()
+{
+    ObjString *b = AS_STRING(pop());
+    ObjString *a = AS_STRING(pop());
+
+    int length = a->length + b->length;
+    char *chars = ALLOCATE(char, length + 1);
+    memcpy(chars, a->chars, a->length);
+    memcpy(chars + a->length, b->chars, b->length);
+    chars[length] = '\0';
+
+    ObjString *result = takeString(chars, length);
+    push(OBJ_VAL(result));
 }
 
 static InterpretResult run()
@@ -46,12 +125,17 @@ static InterpretResult run()
         vm.chunk->constants.values[(byte1 | (byte2 << 8) | (byte3 << 16))]; \
     })
 
-#define BINARY_OP(op)     \
-    do                    \
-    {                     \
-        double b = pop(); \
-        double a = pop(); \
-        push(a op b);     \
+#define BINARY_OP(valueType, op)                        \
+    do                                                  \
+    {                                                   \
+        if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) \
+        {                                               \
+            runtimeError("Operands must be numbers.");  \
+            return INTERPRET_RUNTIME_ERROR;             \
+        }                                               \
+        double b = AS_NUMBER(pop());                    \
+        double a = AS_NUMBER(pop());                    \
+        push(valueType(a op b));                        \
     } while (false)
 
     for (;;)
@@ -83,20 +167,81 @@ static InterpretResult run()
             push(constant);
             break;
         }
-        case OP_ADD:
-            BINARY_OP(+);
+        case OP_NIL:
+            push(NIL_VAL);
             break;
+        case OP_TRUE:
+            push(BOOL_VAL(true));
+            break;
+        case OP_FALSE:
+            push(BOOL_VAL(false));
+            break;
+        case OP_EQUAL:
+        {
+            Value b = pop();
+            Value a = pop();
+            push(BOOL_VAL(valuesEqual(a, b)));
+            break;
+        }
+        case OP_GREATER:
+            BINARY_OP(BOOL_VAL, >);
+            break;
+        case OP_LESS:
+            BINARY_OP(BOOL_VAL, <);
+            break;
+        case OP_ADD:
+        {
+            if (IS_STRING(peek(0)) && IS_STRING(peek(1)))
+            {
+                concatenate();
+            }
+            else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1)))
+            {
+                double b = AS_NUMBER(pop());
+                double a = AS_NUMBER(pop());
+                push(NUMBER_VAL(a + b));
+            }
+            else if (IS_STRING(peek(0)) && IS_NUMBER(peek(1)))
+            {
+                // String is the top of the stack (peek(0)), number is second (peek(1))
+                ObjString *str = AS_STRING(pop());
+                double num = AS_NUMBER(pop());
+                push(OBJ_VAL(stringifyAndConcatenateWithNumber(num, str, true)));
+            }
+            else if (IS_NUMBER(peek(0)) && IS_STRING(peek(1)))
+            {
+                // Number is the top of the stack (peek(0)), string is second (peek(1))
+                double num = AS_NUMBER(pop());
+                ObjString *str = AS_STRING(pop());
+                push(OBJ_VAL(stringifyAndConcatenateWithNumber(num, str, false)));
+            }
+            else
+            {
+                runtimeError(
+                    "Operands must be two numbers, two strings, or a combination of strings and numbers.");
+                return INTERPRET_RUNTIME_ERROR;
+            }
+            break;
+        }
         case OP_SUBTRACT:
-            BINARY_OP(-);
+            BINARY_OP(NUMBER_VAL, -);
             break;
         case OP_MULTIPLY:
-            BINARY_OP(*);
+            BINARY_OP(NUMBER_VAL, *);
             break;
         case OP_DIVIDE:
-            BINARY_OP(/);
+            BINARY_OP(NUMBER_VAL, /);
+            break;
+        case OP_NOT:
+            push(BOOL_VAL(isFalsey(pop())));
             break;
         case OP_NEGATE:
-            push(-pop());
+            if (!IS_NUMBER(peek(0)))
+            {
+                runtimeError("Operand must be a number.");
+                return INTERPRET_RUNTIME_ERROR;
+            }
+            push(NUMBER_VAL(-AS_NUMBER(pop())));
             break;
         case OP_RETURN:
         {
